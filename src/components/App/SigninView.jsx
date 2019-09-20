4004d874f0414f5a7211f4f85d7ea6075a80e14d
PENG YE <yep0608@hotmail.com>
2019-09-20 20:03:27 +1000

    20/09

---
+++ src/components/App/SigninView.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SigninView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFetching: false,
+      email: '',
+      password: '',
+    };
+  }
+
+  render() {
+    
+  }
+}
